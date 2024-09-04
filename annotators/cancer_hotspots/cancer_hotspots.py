51788830b881c33c198026acedd7fe451482a140
aglaiak <kakoulidoumaj7@gmail.com>
2024-09-05 00:42:14 +0200

    .yml and cancer_hotspots file update

--- annotators/cancer_hotspots/cancer_hotspots.py
+++ annotators/cancer_hotspots/cancer_hotspots.py
@@ -4,23 +4,20 @@ from oakvar import BaseAnnotator
 class Annotator(BaseAnnotator):
     def annotate(self, input_data, secondary_data=None):
         _ = secondary_data
-        if not self.cursor:
+        if not self.cursor:            
             return None
-        q = 'select samples from cancer where chrom = "{chrom}" and pos = {pos} and ref = "{ref}" and alt = "{alt}"'.format(
-            chrom=input_data["chrom"],
-            pos=int(input_data["pos"]),
-            ref=input_data["ref_base"],
-            alt=input_data["alt_base"],
+        table = "SNVs"
+        q = 'SELECT samples, qvalue, log10_pvalue FROM {table} WHERE hugo = {hugo} AND aachange = {aachange}'.format(
+            
+            hugo = input_data["hugo_symbol"],
+            aachange = input_data["base_achange"],
+            ref = input_data["base_achange"]
         )
+        if not input_data["aachange"]:
+            table = "INDELs"
+
         self.cursor.execute(q)
         row = self.cursor.fetchone()
-        if row:
-            samples_tmp = str(row[0]).split("|")
-            samples = []
-            for row in samples_tmp:
-                toks = row.split(":")
-                samples.append([toks[0], int(toks[1])])
-            out = {"samples": samples}
-        else:
-            out = None
+        out = {"samples": samples, "qvalue": qvalue, "pvalue" : pvalue}
+
         return out
