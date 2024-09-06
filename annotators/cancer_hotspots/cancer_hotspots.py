d62ce90e641e2ae006aeeeffba8c3098a089a8c4
aglaiak <kakoulidoumaj7@gmail.com>
2024-09-06 15:31:39 +0200

    associating table information correctly

--- annotators/cancer_hotspots/cancer_hotspots.py
+++ annotators/cancer_hotspots/cancer_hotspots.py
@@ -4,20 +4,23 @@ from oakvar import BaseAnnotator
 class Annotator(BaseAnnotator):
     def annotate(self, input_data, secondary_data=None):
         _ = secondary_data
-        if not self.cursor:            
+
+        if not self.cursor:           
             return None
-        table = "SNVs"
-        q = 'SELECT samples, qvalue, log10_pvalue FROM {table} WHERE hugo = {hugo} AND aachange = {aachange}'.format(
-            
-            hugo = input_data["hugo_symbol"],
-            aachange = input_data["base_achange"],
-            ref = input_data["base_achange"]
-        )
-        if not input_data["aachange"]:
+
+        ref_base = input_data["ref_base"]
+        alt_base = input_data["alt_base"]
+
+        if len(ref_base) == len(alt_base) == 1 and ref_base != '-' and alt_base != '-':
+            table = "SNVs"
+        else:
             table = "INDELs"
+
+        q = f"select samples,qvalue, log10_pvalue from {table} where hugo= '{input_data['hugo']}' and achange= '{input_data['achange']}'"
 
         self.cursor.execute(q)
         row = self.cursor.fetchone()
+
         out = {"samples": samples, "qvalue": qvalue, "pvalue" : pvalue}
 
         return out
